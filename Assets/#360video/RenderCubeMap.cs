03f80be09a7ef6ff55f1da20e1a98fac6d85e650
David <Cwlass@gmail.com>
2019-05-08 17:46:59 +0200

    New base for project

---
+++ Assets/#360video/RenderCubeMap.cs
@@ -0,0 +1,40 @@
+﻿using UnityEngine;
+public class RenderCubeMap : MonoBehaviour
+{
+    public RenderTexture cubemapLeftEye;
+    public RenderTexture cubemapRightEye;
+    public RenderTexture equirect;
+    public bool renderStereo = true;
+    public float stereoSeparation = 0.064f;
+    void LateUpdate()
+    {
+        Camera cam = GetComponent<Camera>();
+        if (cam)
+        {
+            Debug.Log("Camera Found");
+        }
+        if (cam == null)
+        {
+            cam = GetComponentInParent<Camera>();
+        }
+        if (cam == null)
+        {
+            Debug.Log("stereo 360 capture node has no camera or parent camera");
+        }
+        if (renderStereo)
+        {
+            cam.stereoSeparation = stereoSeparation;
+            cam.RenderToCubemap(cubemapLeftEye, 63, Camera.MonoOrStereoscopicEye.Left);
+            cam.RenderToCubemap(cubemapRightEye, 63, Camera.MonoOrStereoscopicEye.Right);
+        }
+        else
+        {
+            cam.RenderToCubemap(cubemapLeftEye, 63, Camera.MonoOrStereoscopicEye.Mono);
+        }
+        //optional: convert cubemaps to equirect
+        if (equirect == null)
+            return;
+        cubemapLeftEye.ConvertToEquirect(equirect, Camera.MonoOrStereoscopicEye.Left);
+        cubemapRightEye.ConvertToEquirect(equirect, Camera.MonoOrStereoscopicEye.Right);
+    }
+}
